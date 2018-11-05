4becd9e0fb3c967b7740f2a07dc07012ce648069
Hiraku NAKANO <hiraku@tojiru.net>
2018-11-05 22:32:20 +0900

    ex0102 solve

--- ex0102/echo.go
+++ ex0102/echo.go
@@ -3,9 +3,10 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
 )
 
 func main() {
-	fmt.Println(strings.Join(os.Args[1:], " "))
+	for i, v := range os.Args[1:] {
+		fmt.Printf("%d,%s\n", i, v)
+	}
 }
