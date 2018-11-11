087fad94d7dfb08aa2ba4320d2d3ab4ca852b35f
Hiraku NAKANO <hiraku@tojiru.net>
2018-11-11 14:40:40 +0900

    (ex0104)solve

--- ex0104/dup.go
+++ ex0104/dup.go
@@ -4,13 +4,18 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
 	counts := make(map[string]int)
+	fileNames := make(map[string][]string)
 	files := os.Args[1:]
 	if len(files) == 0 {
 		countLines(os.Stdin, counts)
+		for line, _ := range counts {
+			fileNames[line] = append(fileNames[line], "STDIN")
+		}
 	} else {
 		for _, arg := range files {
 			f, err := os.Open(arg)
@@ -18,21 +23,31 @@ func main() {
 				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
 				continue
 			}
-			countLines(f, counts)
-			f.Close()
+			defer f.Close()
+			c := make(map[string]int)
+			err = countLines(f, c)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+
+			for line, n := range c {
+				counts[line] += n
+				fileNames[line] = append(fileNames[line], f.Name())
+			}
 		}
 	}
 	for line, n := range counts {
 		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
+			fmt.Printf("%d\t%s\t%s\n", n, line, strings.Join(fileNames[line], ", "))
 		}
 	}
 }
 
-func countLines(f *os.File, counts map[string]int) {
+func countLines(f *os.File, counts map[string]int) error {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
 		counts[input.Text()]++
 	}
-	// 注意: input.Err()からのエラーの可能性を無視している
+	return input.Err()
 }
