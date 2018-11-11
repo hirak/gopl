63633303cba176706924f26b0f5081ea8f155371
Hiraku NAKANO <hiraku@tojiru.net>
2018-11-11 15:03:12 +0900

    (ex0105)solve

--- ex0105/lissajous.go
+++ ex0105/lissajous.go
@@ -12,7 +12,10 @@ import (
 	"time"
 )
 
-var palette = []color.Color{color.White, color.Black}
+var palette = []color.Color{
+	color.Black,
+	color.RGBA{0x00, 0xff, 0x00, 0xff}, // green
+}
 
 const (
 	whiteIndex = 0 // パレットの最初の色
