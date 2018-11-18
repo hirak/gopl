abf1430a7d2b5a7b7c338e47a02c3d9f38f9eb09
Hiraku NAKANO <hiraku@tojiru.net>
2018-11-18 14:23:52 +0900

    (ex0112)import lissajous

--- ex0112/server.go
+++ ex0112/server.go
@@ -1,16 +1,59 @@
 package main
 
 import (
-	"fmt"
+	"image"
+	"image/color"
+	"image/gif"
+	"io"
 	"log"
+	"math"
+	"math/rand"
 	"net/http"
+	"time"
 )
 
 func main() {
+	rand.Seed(time.Now().UTC().UnixNano())
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+	lissajous(w)
+}
+
+var palette = []color.Color{
+	color.Black,
+	color.RGBA{0x00, 0xff, 0x00, 0xff}, // green
+}
+
+const (
+	blackIndex = iota
+	greenIndex
+)
+
+func lissajous(out io.Writer) {
+	const (
+		cycles  = 5     // 発振器xが完了する周回の回数
+		res     = 0.001 // 回転の分解能
+		size    = 100   //画像キャンバスは[-size..+size]の範囲を扱う
+		nframes = 64    // アニメーションフレーム数
+		delay   = 8     // 10ms単位でのフレーム間の遅延
+	)
+	freq := rand.Float64() * 3.0 // 発振器yの相対周波数
+	anim := gif.GIF{LoopCount: nframes}
+	phase := 0.0 // 位相差
+	for i := 0; i < nframes; i++ {
+		rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+		img := image.NewPaletted(rect, palette)
+		for t := 0.0; t < cycles*2*math.Pi; t += res {
+			x := math.Sin(t)
+			y := math.Sin(t*freq + phase)
+			img.SetColorIndex(size+int(x*size+0.5), size+int(y*size+0.5), greenIndex)
+		}
+		phase += 0.1
+		anim.Delay = append(anim.Delay, delay)
+		anim.Image = append(anim.Image, img)
+	}
+	gif.EncodeAll(out, &anim) // 注意：エンコードエラーを無視
 }
