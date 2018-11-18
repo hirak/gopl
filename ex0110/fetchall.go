27be607f6b4bbbdabc1039faf31867d969f0182b
Hiraku NAKANO <hiraku@tojiru.net>
2018-11-18 12:24:15 +0900

    (ex0110)copy from textbook

---
+++ ex0110/fetchall.go
@@ -0,0 +1,41 @@
+// fetchallは、URLを並行に取り出して、時間と大きさを表示します。
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch) // ゴルーチンを開始
+	}
+	for range os.Args[1:] {
+		fmt.Println(<-ch) // chチャネルから受信
+	}
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err) // chチャネルへ送信
+		return
+	}
+
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
+}
