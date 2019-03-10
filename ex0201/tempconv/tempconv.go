84319e570f91f5d510b0d557a9ab765f312463c4
Hiraku NAKANO <hiraku@tojiru.net>
2019-03-10 11:55:18 +0900

    solve 0201

--- ex0201/tempconv/tempconv.go
+++ ex0201/tempconv/tempconv.go
@@ -5,6 +5,7 @@ import "fmt"
 type (
 	Celsius    float64
 	Fahrenheit float64
+	Kelvin     float64
 )
 
 const (
@@ -21,10 +22,30 @@ func (f Fahrenheit) String() string {
 	return fmt.Sprintf("%g°F", f)
 }
 
+func (k Kelvin) String() string {
+	return fmt.Sprintf("%gK", k)
+}
+
 func CToF(c Celsius) Fahrenheit {
 	return Fahrenheit(c*9/5 + 32)
 }
 
 func FToC(f Fahrenheit) Celsius {
 	return Celsius((f - 32) * 5 / 9)
+}
+
+func CToK(c Celsius) Kelvin {
+	return Kelvin(c + 273.15)
+}
+
+func KToC(k Kelvin) Celsius {
+	return Celsius(k - 273.15)
+}
+
+func FToK(f Fahrenheit) Kelvin {
+	return CToK(FToC(f))
+}
+
+func KToF(k Kelvin) Fahrenheit {
+	return CToF(KToC(k))
 }
